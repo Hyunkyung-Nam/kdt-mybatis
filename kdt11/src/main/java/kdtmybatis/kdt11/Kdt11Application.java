a8762a740122119603fdbbb7ab3a227a66e9a1f1
Hyunkyung-Nam <gusrud13579@gmail.com>
2024-04-27 13:41:19 +0900

    0427 mybatis강의 및 실습 코드

---
+++ kdt11/src/main/java/kdtmybatis/kdt11/Kdt11Application.java
@@ -0,0 +1,13 @@
+package kdtmybatis.kdt11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kdt11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kdt11Application.class, args);
+	}
+
+}
